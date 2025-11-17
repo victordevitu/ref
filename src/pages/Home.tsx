384950e7b3a641992d36b2198b0ffe771245422e
victormatia <fvictor1705@gmail.com>
2025-11-17 16:17:17 -0300

    feat: finaliza página de Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,7 @@ import { Mouse } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div
-      id="home"
-      className="relative flex h-screen flex-col justify-center gap-8 overflow-hidden p-4 md:justify-end-safe md:p-16">
+    <div id="home" className="relative flex h-screen flex-col justify-end-safe gap-8 overflow-hidden p-4 pb-24 md:p-16">
       <video
         className="absolute top-0 left-0 z-0 h-screen w-full object-cover"
         src="/videos/bg-video.mp4"
@@ -14,17 +12,17 @@ export default function Home() {
         playsInline
       />
 
-      <div className="to absolute inset-0 bg-gradient-to-t from-black from-20% to-black/40"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(20deg,rgba(0,0,0,0.8)_10%,transparent)]"></div>
 
       <div className="z-10 text-white">
-        <h1 className="text-6xl font-bold uppercase max-sm:text-4xl">
+        <h1 className="text-4xl font-bold uppercase md:text-6xl">
           A <span className="text-outline">tela</span> é nossa <br /> janela para o{' '}
           <span className="text-outline">mundo</span>
         </h1>
       </div>
       <div className="z-10 flex animate-pulse gap-2">
-        <Mouse size={24} className="text-base text-zinc-500" />
-        <p className="text-lg font-normal text-zinc-500">Role para baixo</p>
+        <Mouse size={24} className="text-base text-zinc-400" />
+        <p className="text-lg font-normal text-zinc-400">Role para baixo</p>
       </div>
     </div>
   );
