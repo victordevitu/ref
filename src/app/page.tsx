3c39bacb636d12308e1891220e8acf23730daaac
victormatia <fvictor1705@gmail.com>
2025-12-29 13:35:34 -0300

    feat: adiciona páginas de About e Cases em App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import About from '@/pages/About';
 import Cases from '@/pages/Cases';
+import Contact from '@/pages/Contact';
 import Home from '@/pages/Home';
 
 export default function App() {
   return (
     <div>
       <Home />
+      <About />
       <Cases />
+      <Contact />
     </div>
   );
 }
